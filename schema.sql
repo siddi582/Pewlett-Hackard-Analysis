4ac0b1cf4e57dec3e0afdfad1d9d7fb8b6372589
Shehryar Siddiquia <shehryar.siddiqui@mail.utoronto.ca>
2022-12-05 18:32:31 -0500

    updates

--- schema.sql
+++ schema.sql
@@ -32,6 +32,28 @@ CREATE TABLE salaries (
 	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
 	PRIMARY KEY (emp_no)
 );
+CREATE TABLE dept_emp (
+dept_no VARCHAR(4) NOT NULL,
+emp_no INT NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE Titles (
+emp_no INT NOT NULL,
+title VARCHAR(4) NOT NULL,
+from_date DATE NOT NULL,
+to_date DATE NOT NULL,
+FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+PRIMARY KEY (emp_no, title, from_date)
+);
+	
 SELECT * FROM departments;
 	
+SELECT * FROM dept_manager;
 );
+
+DROP TABLE employees CASCADE;
+DROP TABLE dept_emp CASCADE;
+DROP TABLE Titles CASCADE;
